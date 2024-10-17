005dc6ca87cc066e51fc8aa43a760b634bf5c4e2
Bode <jry.bode@gmail.com>
2024-10-18 03:19:15 +0700

    almost there

---
+++ check.py
@@ -0,0 +1,28 @@
+# import hailo_platform.pyhailort as pyhailort
+
+# print(dir(pyhailort))
+# import hailo_platform.pyhailort as pyhailort
+
+# print(dir(pyhailort.control_object))
+# print(dir(pyhailort.hw_object))
+# import hailo_platform.pyhailort as pyhailort
+
+# print(dir(pyhailort.hw_object.PcieDevice))
+# print(dir(pyhailort.hw_object.ConfiguredNetwork))
+# import hailo_platform.pyhailort as pyhailort
+
+# print(dir(pyhailort.pyhailort.VDevice))
+import hailo_platform.pyhailort as pyhailort
+
+# Get available methods from pyhailort and VDevice
+pyhailort_methods = dir(pyhailort)
+vdevice_methods = dir(pyhailort.pyhailort.VDevice)
+
+# Write output to text files
+with open("pyhailort_methods.txt", 'w') as f:
+    f.write('\n'.join(pyhailort_methods))
+
+with open("vdevice_methods.txt", 'w') as f:
+    f.write('\n'.join(vdevice_methods))
+
+print("Methods saved to pyhailort_methods.txt and vdevice_methods.txt")
